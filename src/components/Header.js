074bdb063b317541f26cfc3b515d3f87d2f410a1
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-12-12 11:39:29 -0500

    adding logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import './Header.css';
+import punkLogo from '../assets/header/punkLogo.png';
 
 const Header = () => {
     return (
         <div className="header">
             <div className="logoContainer">
-                <img src={punkLogo} className='punkLogo' alt="" />
+                <img src={punkLogo} className='punkLogo' alt="punklogo" />
         </div>
         </div>
     );    
