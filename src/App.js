f66a938b5d481e4fc46141072ae8371b79f5db67
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-12-12 16:31:36 -0500

    adding imgaes and props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
+import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <CollectionCard
+        id={0}
+        name={"Bandana Punk"}
+        traits={[{ value: 7 }]}
+        image="https://nftlabs.mypinata.cloud/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"
+      />
     </div>
   );
 }
---
+++ src/components/CollectionCard.js
@@ -0,0 +1,19 @@
+import weth from '../assets/weth.png';
+
+const CollectionCard = ({ id, name, traits, image }) => {
+  return (
+    <div className="collection-card">
+      <img src={image} alt="" />
+      <div className="details"></div>
+      <div className="name">
+              {name} <div className="id">#{id}</div>
+          </div>
+          <div className="priceContainer">
+              <img src={weth} className="wethImage" alt="" />
+              <div className="price">{traits[0]?.value}</div>
+          </div>
+    </div>
+  );
+};
+
+export default CollectionCard;
