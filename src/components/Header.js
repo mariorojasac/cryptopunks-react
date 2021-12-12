6cce1d704a9edf07f9fcd947d4077d3bd8c3b843
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-12-12 15:08:57 -0500

    add login button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,40 @@
-import './Header.css';
-import punkLogo from '../assets/header/punkLogo.png';
+import "./Header.css";
+import punkLogo from "../assets/header/punkLogo.png";
+import searchIcon from "../assets/header/search.png";
+import themeSwitchIcon from "../assets/header/theme-switch.png";
 
 const Header = () => {
-    return (
-        <div className="header">
-            <div className="logoContainer">
-                <img src={punkLogo} className='punkLogo' alt="punklogo" />
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="punklogo" />
+      </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} alt="searchicon" />
         </div>
-        </div>
-    );    
-}
+        <input placeholder="Collection" className="searchInput"/>
+          </div>
+          
+          <div className="headerItems">
+              <p>Drops</p>
+              <p>Marketplace</p>
+              <p>Create</p>
+          </div>
+
+          <div className="headerActions">
+              <div className="themeSwitchContainer">
+                  <img src={themeSwitchIcon} alt="" />
+              </div>
+          </div>
+
+          <div className="loginButton">
+                GET IN
+          </div>
+          
+    </div>
+  );
+};
 
 export default Header;
