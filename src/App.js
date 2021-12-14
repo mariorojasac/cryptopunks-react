70a95b9b3b5196364271245a1066fb9ded98c051
mariorojasac <77466868+mariorojasac@users.noreply.github.com>
2021-12-14 08:21:26 -0500

    adding styles and props punklist

--- src/App.js
+++ src/App.js
@@ -2,23 +2,29 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import CollectionCard from "./components/CollectionCard";
 import Header from "./components/Header";
-import {useState, useEffect} from "react";
+import axios from "axios";
+import PunkList from "./components/PunkList";
+import './components/PunkList.css';
 
 function App() {
+  const [ punkListData, setPunkListData ] = useState([]);
 
-
-
-
+  useEffect(() => {
+    const getMyNfts = async () => {
+      const openseaData = await axios.get(
+        "https://testnets-api.opensea.io/assets?asset_contract_address=0x12e05e76c06527A169af05d77EcbDAe345D8d06d&order_direction=asc"
+      );
+      console.log(openseaData.data.assets);
+      setPunkListData(openseaData.data.assets);
+    };
+    return getMyNfts();
+  }, []);
 
   return (
     <div className="app">
       <Header />
-      <CollectionCard
-        id={0}
-        name={"Bandana Punk"}
-        traits={[{ value: 7 }]}
-        image="https://nftlabs.mypinata.cloud/ipfs/bafkreiasgnibw32isqfyjnv5lhj4wxzp6izayculjlo4ampmmfla3j4tj4"
-      />
+
+      <PunkList punkListData={punkListData} />
     </div>
   );
 }
